b8d00c904fef3058274769267ed580979473251a
Edgar Santos <edfil221@gmail.com>
2019-05-19 20:10:53 +0200

    Start Engine with a simple Scene

--- src/ecs/ECS.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-using GlobalHandle = uint32_t;
-using LocalHandle = uint16_t;
--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,47 @@
-#include <chrono>
-#include "ecs/TransformSystem.h"
+#include <Engine.hpp>
+#include <Constants.hpp>
+#include <file/FileManager.hpp>
+#include <SDL_log.h>
+#include <scene/Scene.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <string>
+
+class MyScene : public Scene {
+	void onCreated() override {	
+		SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "My onCreate()");
+	}
+
+	void onDestroy() override {
+		SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "My onDestroy()");
+	}
+
+	void update(float) override {	
+		SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "My update()");
+	}
+};
+
+int main()
+{
+	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+
+    FileManager::initialize();
+
+	//std::string fullPath = FileManager::instance()->fullPathForFile("ImageTest->png");
+	//std::vector<char> data = FileManager::instance()->loadFile("ImageTest->png");
+
+    //int x, y, channels_in_file;
+    //stbi_uc* result = stbi_load(fullPath->c_str(), &x, &y, &channels_in_file, 4);
+    //stbi_image_free(result);
 
 
-#define BENCHMARK(__PARAM__) \
-{\
-    auto start = std::chrono::high_resolution_clock::now();\
-    __PARAM__\
-    auto elapsed =  std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - start);\
-    std::cout << "[TIME] Took "<< elapsed.count() << " stuffs" << std::endl;\
+    Engine* engine = new Engine();
+    if(engine->initialize()) {
+		engine->setScene(std::make_unique<MyScene>());
+        engine->run();
+    }
+
+    return 0;
 }
-
-int main() {
-    TransformSystem system;
-    system.initWithCapacity(100);
-
-    const size_t phase1 = 99;
-    GlobalHandle phase1Handles[phase1];
-    for (size_t i = 0; i < phase1; i++) {
-        phase1Handles[i] = system.createComponent();
-    }
-
-    system.createComponent();
-    system.createComponent();
-
-    const size_t phase2 = 3;
-    GlobalHandle phase2Handles[phase2] = { phase1Handles[12], phase1Handles[1], phase1Handles[30]};
-    for (size_t i = 0; i < phase2; i++) {
-        system.destroyComponent(phase2Handles[i]);
-    }
-
-    const size_t phase3 = 54;
-    GlobalHandle phase3Handles[phase3];
-    for (size_t i = 0; i < phase3; i++) {
-        phase3Handles[i] = system.createComponent();
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#include <Engine->hpp>
-//#include <Constants->hpp>
-//#include <file/FileManager->hpp>
-//#include <SDL_log->h>
-//
-//#define STB_IMAGE_IMPLEMENTATION
-//#include <stb_image->h>
-//
-//#include <string>
-//
-//int main()
-//{
-//	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
-//
-//    FileManager::initialize();
-//
-//	//std::string fullPath = FileManager::instance()->fullPathForFile("ImageTest->png");
-//	//std::vector<char> data = FileManager::instance()->loadFile("ImageTest->png");
-//
-//    //int x, y, channels_in_file;
-//    //stbi_uc* result = stbi_load(fullPath->c_str(), &x, &y, &channels_in_file, 4);
-//    //stbi_image_free(result);
-//
-//
-//    Engine* engine = new Engine();
-//    if(engine->initialize()) {
-//        engine->run();
-//    }
-//
-//    return 0;
-//}
