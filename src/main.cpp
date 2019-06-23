2138a1009705639cdaaaf1096cb1a2aea2186889
Edgar Santos <edfil221@gmail.com>
2019-06-23 22:47:02 +0200

    More stuff, but going nowhere

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,61 @@
 #include <Engine.hpp>
 #include <Constants.hpp>
-#include <file/FileManager.hpp>
-#include <SDL_log.h>
-#include <TextureManager.hpp>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <SDL_log.h>
 
 #include <string>
 #include <ctime>
 
+#include <Scene.hpp>
+#include <math/Matrix4f.hpp>
+
+#define NUM_ENTITIES 100000
+class MyScene : public Scene {
+public:
+
+    EntityID m_entities[NUM_ENTITIES];
+
+    void onCreated() override {
+        _engine->entityManager().initWithCapacity(10);
+    }
+
+    void update(float) override {
+        for (auto& m_entity : m_entities) {
+            m_entity = _engine->entityManager().createEntity();
+        }
+
+        EntityID entitiesToDestroy[] = { m_entities[0], m_entities[5], m_entities[9] };
+        for (EntityID& entity : entitiesToDestroy) {
+            _engine->entityManager().destroyEntity(entity);
+        }
+
+        for (auto& m_entity : m_entities) {
+            _engine->entityManager().createEntity();
+        }
+
+        _engine->shutdown();
+    }
+
+    void onDestroy() override {
+        for (auto& m_entity : m_entities) {
+            _engine->entityManager().destroyEntity(m_entity);
+        }
+    }
+
+};
+
 int main()
 {
 	srand(time(nullptr));
-
 	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
-
-    FileManager::initialize();
-
-	//std::string fullPath = FileManager::instance()->fullPathForFile("ImageTest->png");
-	//std::vector<char> data = FileManager::instance()->loadFile("ImageTest->png");
-
-    //int x, y, channels_in_file;
-    //stbi_uc* result = stbi_load(fullPath->c_str(), &x, &y, &channels_in_file, 4);
-    //stbi_image_free(result);
-
 
     Engine* engine = new Engine();
     if(engine->initialize()) {
+        engine->setScene(std::make_unique<MyScene>());
         engine->run();
     }
+
+    delete engine;
 
     return 0;
 }
